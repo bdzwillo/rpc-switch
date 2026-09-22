3afbd86dc310e894cdf461625b79f0cf438beb4a
Barnim Dzwillo <dzwillo@strato.de>
2026-09-22 18:26:54 +0200

    rpcswitch: add a usage example and a startable example config
    - README shows the config, the accounts and a rpctiny round trip
    - doc/examples/config.pl starts as shipped: tls and clientcert are off
    - load Auth::Password: v2 renamed Auth::Passwd
    - use address 127.0.0.1 (mojo listens on all interfaces without it)
    - the example needs PERL5LIB when make deps installed into local dir
    - the client dist ships rpctiny as an example but does not install it

--- doc/examples/config.pl
+++ doc/examples/config.pl
@@ -6,34 +6,36 @@ $cfg = {
 	methods => 'methods.pl',
 	listen => [
 		{
-			# dislay name for this local endpoint
+			# display name for this local endpoint
 			name => 'default port',
-			# default values:
-			#address => '127.0.0.1',
+			# without an address mojo listens on all interfaces
+			address => '127.0.0.1',
+			# default value:
 			#port => 6551,
 			# auth => ['all'],
 		},
-		{
-			address => '127.0.0.1',
-			port => 6850,
-			# adding a key enables tls
-			tls_cert => 'rpcswitch.crt',
-			tls_key => 'rpcswitch.pem',
-			# adding a ca enables client cerfiticate checking
-			tls_ca => 'myCA.crt',
-		},
+		# a second endpoint, uncomment to enable tls
+		#{
+		#	address => '127.0.0.1',
+		#	port => 6850,
+		#	# adding a key enables tls
+		#	tls_cert => 'rpcswitch.crt',
+		#	tls_key => 'rpcswitch.pem',
+		#	# adding a ca enables client certificate checking
+		#	tls_ca => 'myCA.crt',
+		#},
 	],
 	auth => {
 		# authentication methods supported
-		password => 'RPC::Switch::Auth::Passwd',
-		clientcert => 'RPC::Switch::Auth::ClientCert',
+		password => 'RPC::Switch::Auth::Password',
+		#clientcert => 'RPC::Switch::Auth::ClientCert',
 	},
 	# per authentication method configuration
 	'auth|password' => {
 		pwfile => 'switch.passwd'
 	},
-	'auth|clientcert' => {
-		cnfile => 'switch.cnfile'
-	},
+	#'auth|clientcert' => {
+	#	cnfile => 'switch.cnfile'
+	#},
 };
 
